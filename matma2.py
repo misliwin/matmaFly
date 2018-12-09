b84173bb2a77894998c2246640eee84139bb8d18
misliwin <milosz.sliwinski96@gmail.com>
2018-12-09 22:10:03 +0100

    push

--- matma2.py
+++ matma2.py
@@ -1,18 +1,54 @@
+cities = open("output.txt", 'r')
+ofile = open("output_filted.dat", 'w')
+
+Efile = open("E_list_filtered.txt", 'w')
+V_list = []
+V_listf = open("V_list_filtered.txt", 'r')
+for line in V_listf:
+    V_list.append(line[0:3])
+
+
+
+i =1
 E_list = []
-E_listf = open("C:\\Users\\user\\Desktop\\learning\\loty_matma\\E_list2.txt", 'r')
+E_listf = open("E_list2.txt", 'r')
+for line in E_listf:
+    if line[0:3] in V_list and line[3:6] in V_list:
+        Efile.write(line[0:6] + " " + str(i) + '\n')
+        i = i+1
+        E_list.append(line[0:6])
+print("Elist is filtered")
+
+
+for line in cities:
+    if "2019-01-1" in line:
+        if line[0:3] + line[5:8] in E_list:
+            ofile.write(line)
+print("New output with filtered days is saved")
+
+cities.close()
+E_listf.close()
+V_listf.close()
+Efile.close()
+ofile.close()
+
+
+
+E_list = []
+E_listf = open("E_list_filtered.txt", 'r')
 for line in E_listf:
     E_list.append(line)
 
-cities = open("C:\\Users\\user\\Desktop\\learning\\loty_matma\\output.txt", 'r')
+cities = open("output_filted.dat", 'r')
 
 
 V_list = []
-V_listf = open("C:\\Users\\user\\Desktop\\learning\\loty_matma\\V_list2.txt", 'r')
+V_listf = open("V_list_filtered.txt", 'r')
 for line in V_listf:
     V_list.append(line)
 
 
-ofile = open("C:\\Users\\user\\Desktop\\learning\\loty_matma\\data_file_new.txt", 'w')
+ofile = open("data.dat", 'w')
 
 srcCity = ""
 dstCity = ""
@@ -48,9 +84,12 @@ for i, node in enumerate(E_list):
 ofile.write("""/* Input data */
 data;
 
-param V_count := 398;
-param E_count := 4582;
-param DAY_count := 20;
+param V_count := 200;
+param E_count := 3915;
+param DAY_count := 11;
+param M := 3;
+param STAY := 2;
+param S := 1;
 
 param : A :=\n""")
 
@@ -69,19 +108,20 @@ for line in cities:
     delta = delta.days
     #print(srcCity + dstCity + str(cost))
     if srcCity + dstCity + str(delta) not in all_list:
-        #all_list.append(srcCity + dstCity + str(delta))
+        all_list.append(srcCity + dstCity + str(delta))
         ofile.write(" " + str(E_iter_list[srcCity + dstCity]) + "," + str(V_iter_list[srcCity]) + "," + str(delta+1) +"    1\n")
         #ofile.write(" " + str(E_iter_list[srcCity + dstCity]) + "," + str(V_iter_list[srcCity]) + "," + str(delta) + "    1\n")
-
+print("save param A")
 cities.close()
-cities = open("C:\\Users\\user\\Desktop\\learning\\loty_matma\\output.txt", 'r')
+cities = open("output_filted.dat", 'r')
 i=0
-ofile.write("""
+ofile.write(""";
 
 
 
 param : B :=\n""")
-
+del all_list
+all_list = []
 for line in cities:
     i = i + 1
     list = line.split(";")
@@ -96,18 +136,20 @@ for line in cities:
     delta = delta.days
     # print(srcCity + dstCity + str(cost))
     if srcCity + dstCity + str(delta) not in all_list:
-        # all_list.append(srcCity + dstCity + str(delta))
+        all_list.append(srcCity + dstCity + str(delta))
         ofile.write( " " + str(E_iter_list[srcCity + dstCity]) + "," + str(V_iter_list[dstCity]) + "," + str(delta+1) + "    1\n")
 
 
-
+print("save param B")
 cities.close()
-cities = open("C:\\Users\\user\\Desktop\\learning\\loty_matma\\output.txt", 'r')
+cities = open("output_filted.dat", 'r')
 
 ofile.write(""";
 
 param : C :=\n""")
-
+del all_list
+all_list = []
+i=0
 for line in cities:
     i=i+1
     list = line.split(";")
@@ -120,6 +162,7 @@ for line in cities:
     delta = date - day1
     delta = delta.days
     cost = list[3][1:-1]
+
     """flyline = srcCity + dstCity
     for item in E_list:
         if flyline in item:
@@ -129,10 +172,12 @@ for line in cities:
             number = 0"""
     #ofile.write(" " + number + "," + str(delta+1) + "    1\n")
 
-    ofile.write(" " + str(E_iter_list[srcCity + dstCity]) + "," + str(delta+1) + "    " + cost + "\n")
+    if str(E_iter_list[srcCity + dstCity]) + "," + str(delta) not in all_list:
+        all_list.append(str(E_iter_list[srcCity + dstCity]) + "," + str(delta))
+        ofile.write(" " + str(E_iter_list[srcCity + dstCity]) + "," + str(delta+1) + "    " + cost + "\n")
 
 
-
+print("save param C")
 
 
 
--- model.mod
+++ model.mod
@@ -1,44 +1,46 @@
 # MPSiS 2018/2019
 # Model UFAP, N/L
 #Input
-param M >= 1, integer; #liczba maist do odiwedzenia
-param STAY >= 1, integer; #jak dlugo pobyt
-param S >= 1, integer; #miasto z ktorego lecimy
+
+#to run model
+#glpsol -m model.mod -d data.dat -o myprob.sol
+
+param M >= 1 integer, default 2; #liczba maist do odiwedzenia
+param STAY >= 1 integer, default 2; #jak dlugo pobyt
+param S >= 1 integer, default 1; #miasto z ktorego lecimy
 
 /* Number of vertexes, edges, dispositions */
-param V_count, integer, >= 0;
-param E_count, integer, >= 0;
-param DAY_count, integer, >= STAY;
+param V_count integer, default 52, >= 0;
+param E_count integer, default 551, >= 0;
+param DAY_count integer, default 11, >= STAY;
 
 /* Sets of vertexes, edges and dispositions */
 set V, default {1..V_count};
 set E, default {1..E_count};
 set DAYS, default {1..DAY_count};
-set DAYSWITHOUTSTAY, default {1..DAY_count-STAY};
+set DAYSWITHOUTSTAY default {1..DAY_count-STAY};
 
 
 /* Aev, Bev as params */
-param A{e in E, v in V, day in DAYS}, >= 0, binary, default 0;
-param B{e in E, v in V, day in DAYS}, >= 0, binary, default 0;
+param A{e in E, v in V, day in DAYS} binary, default 0;
+param B{e in E, v in V, day in DAYS} binary, default 0;
 
 
 /* Cost */
 param C{e in E, day in DAYS} >= 0, default 90000;
 
 /* Decision variables */
-var x{e in E, day in DAYS} >= 0, binary; #binarnie wybieranie lotow
+var x{e in E, day in DAYS} binary; #binarnie wybieranie lotow
 
 
 /* Objective function 'z' */
 minimize z: sum{e in E, day in DAYS} x[e,day]*C[e,day];
 
 /* Constraints */
-#s.t. c1{d in D, v in V : v == s[d]} : sum{e in E} (A[e,v]*x[e,d] - B[e,v]*x[e,d]) == h[d];
-#s.t. c2{d in D, v in V : v <> s[d] and v <> t[d]} : sum{e in E} (A[e,v]*x[e,d] - B[e,v]*x[e,d]) == 0;
-
 s.t. c1{v in V} : sum{e in E, day in DAYS} (A[e,v,day]*x[e,day] - B[e,v,day]*x[e,day]) == 0;
 s.t. c2 : sum{e in E, day in DAYS} x[e,day] == M;
 
-s.t. c3{v in V : v <> S, day in DAYSWITHOUTSTAY} : sum{e in E} (A[e,v,day]*x[e,day] - B[e,v,day+STAY]*x[e,day+STAY]) == 0; #except first and last
+
+s.t. c3{day in DAYSWITHOUTSTAY, v in V : v <> S} : sum{e in E} (A[e,v,day+STAY]*x[e,day+STAY] - B[e,v,day]*x[e,day]) == 0; #except first and last
 
 end;
