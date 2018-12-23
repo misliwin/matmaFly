63e3e7ac0d1264a1eb80366f74b0d152f1d69c67
misliwin <milosz.sliwinski96@gmail.com>
2018-12-23 17:49:17 +0100

    fds

---
+++ modify_output_matma.py
@@ -0,0 +1,13 @@
+i_file = open("myprob.sol", 'r')
+o_file = open("output_modified.sol", 'w')
+
+for i,line in enumerate(i_file):
+    if i < 7:
+        o_file.write(line)
+    if "x[" in line and "1     " in line:
+        city_id = line[9:line.find(',')]
+        for city in open("E_list2.txt", 'r'):
+            if " " + city_id + "\n" in city:
+                o_file.write("From: {}  To: {}   Day: {}\n".format(city[0:3],city[3:6], line[line.find(',')+1:line.find(']')]))
+                print(city[0:3] +" " + city[3:6])
+        print(city_id)
