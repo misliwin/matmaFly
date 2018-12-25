1c8d097940c693dea7b37f1c6cf3c7cdce180f14
misliwin <milosz.sliwinski96@gmail.com>
2018-12-25 20:01:57 +0100

    fds

--- modify_output_matma.py
+++ modify_output_matma.py
@@ -1,12 +1,12 @@
-i_file = open("myprob.sol", 'r')
-o_file = open("output_modified.sol", 'w')
+i_file = open("myprob2.sol", 'r')
+o_file = open("output_modified2.sol", 'w')
 
 for i,line in enumerate(i_file):
     if i < 7:
         o_file.write(line)
     if "x[" in line and "1     " in line:
         city_id = line[9:line.find(',')]
-        for city in open("E_list2.txt", 'r'):
+        for city in open("E_list_filtered.txt", 'r'):
             if " " + city_id + "\n" in city:
                 o_file.write("From: {}  To: {}   Day: {}\n".format(city[0:3],city[3:6], line[line.find(',')+1:line.find(']')]))
                 print(city[0:3] +" " + city[3:6])
---
+++ output_modified2.sol
@@ -0,0 +1,10 @@
+Problem:    model
+Rows:       1950
+Columns:    47138 (47138 integer, 47138 binary)
+Non-zeros:  168861
+Status:     INTEGER OPTIMAL
+Objective:  z = 38 (MINimum)
+
+From: KBP  To: WRO   Day: 8
+From: WRO  To: GDN   Day: 10
+From: GDN  To: KBP   Day: 12
